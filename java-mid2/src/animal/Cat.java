cb3237ffe93684097bcce4db227c5e165f7dec44
sanggak2 <blizard0987@gmail.com>
2025-03-04 22:13:54 +0900

    refactoring

---
+++ java-mid2/src/animal/Cat.java
@@ -0,0 +1,13 @@
+package animal;
+
+public class Cat extends Animal {
+
+    public Cat(String name, int size) {
+        super(name, size);
+    }
+
+    @Override
+    public void sound() {
+        System.out.println("YaaOng");
+    }
+}
