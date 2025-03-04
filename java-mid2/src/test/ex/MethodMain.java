75fc09cde6eca7e50a73889023aade2402470f00
sanggak2 <blizard0987@gmail.com>
2025-03-04 22:13:54 +0900

    refactoring

---
+++ java-mid2/src/test/ex/MethodMain.java
@@ -0,0 +1,33 @@
+package test.ex;
+
+import animal.Cat;
+import animal.Dog;
+
+public class MethodMain {
+    public static void main(String[] args) {
+        /**
+         * Integer i = 1;
+         *         GenericMethod.objMethod(i);
+         *
+         *         //타입명시전달
+         *         System.out.println(
+         *                 GenericMethod.<Integer>genericMethod(i));
+         *         //인자값으로 타입추론 가능
+         *         System.out.println(
+         *                 GenericMethod.numberMethod(i));
+         */
+
+        //////////////////////////////////////////////////
+
+        Dog dog = new Dog("MeongMeong2", 100);
+        Cat cat = new Cat("NyaOOng2", 50);
+
+        ComplexBox<Dog> hospital = new ComplexBox<>();
+        hospital.setAnimal(dog);
+
+        //제네릭 메서드에 있는 타입이 사용됨
+        System.out.println("ReturnCat : "+hospital.printAndReturn(cat));
+
+
+    }
+}
